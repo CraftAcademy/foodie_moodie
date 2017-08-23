03ae906921c803e3d44ce1bc589fa2b416f9d4e2
Carissa <carissa@edgren.nu>
2017-08-23 15:05:28 +0200

    fixes stuff ;-)

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -7,15 +7,19 @@ def create
   @amount = @order.total.to_i
 
   customer = Stripe::Customer.create(
-    :email => params[:stripeEmail],
-    :source  => params[:stripeToken]
+    email: params[:stripeEmail],
+    source: params[:stripeToken]
   )
 
   charge = Stripe::Charge.create(
-    :customer    => customer.id,
-    :amount      => @amount,
-    :description => 'Order ' + @order.id.to_s,
-    :currency    => 'sek'
+    customer:     customer.id,
+    amount:       @amount,
+    description:  'Order ' + @order.id.to_s,
+    currency:     'sek'
   )
 
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
 end
