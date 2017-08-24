aeecd00175abdf962ff04cb0c11df82ea3de320c
Carissa <carissa@edgren.nu>
2017-08-24 21:13:13 +0200

    Adds customer information fields to order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170819124013) do
+ActiveRecord::Schema.define(version: 20170824190553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(version: 20170819124013) do
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "address1"
+    t.string "address2"
+    t.string "postal_code"
+    t.string "city"
+    t.string "phone_number"
+    t.string "email"
   end
 
   create_table "restaurants", force: :cascade do |t|
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,6 +1,13 @@
 RSpec.describe Order, type: :model do
   it { is_expected.to have_db_column :created_at }
   it { is_expected.to have_db_column :updated_at }
+  it { is_expected.to have_db_column :name }
+  it { is_expected.to have_db_column :address1 }
+  it { is_expected.to have_db_column :address2 }
+  it { is_expected.to have_db_column :postal_code }
+  it { is_expected.to have_db_column :city }
+  it { is_expected.to have_db_column :phone_number }
+  it { is_expected.to have_db_column :email }
 
   it 'has 0 tax' do
     expect(subject.tax_pct).to eq 0
