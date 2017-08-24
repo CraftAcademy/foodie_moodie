3a6593cbab007f7cdd59511b0b505c83b3385974
satorinic <niccarlsson@gmail.com>
2017-08-24 09:39:35 +0200

    add steps

---
+++ features/step_definitions/delete_item_steps.rb
@@ -0,0 +1,7 @@
+Given(/^given I see "([^"]*)" and "([^"]*)" on the "([^"]*)" page$/) do |arg1, arg2, arg3|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click on the "([^"]*)" button next to "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
